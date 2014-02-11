aada8e272c3c0121bf17c5c5142fcc0df42ed269
Chris Hluchan <chluchan@unifocus.com>
2014-02-11 11:09:58 -0700

    added an interview section called intro which assumes programming knowlege but not java specific

---
+++ src/org/interview/intro/RunningAverageCalculator.java
@@ -0,0 +1,11 @@
+package org.interview.intro;
+
+public class RunningAverageCalculator {
+    public void add(Double number) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    public double runningAverage() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
